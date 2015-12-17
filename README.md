f17d471a6afc998e247a84b2514959a5be6ee6b2
Jakob Rosenberg <jakobrosenberg@gmail.com>
2015-12-17 13:22:44 +0100

    fixed binding issue

--- README.md
+++ README.md
@@ -5,38 +5,31 @@
 
 Installation:
 ```javascript
-var VueMeteorData = require ('vue-meteor-data');
-Vue.use(VueMeteorData);
-      
+var VueMeteorData = require ('vue-meteor-data')
+Vue.mixin(VueMeteorData)      
 ```
  
  
-Usage example 1:
-  
+Usage example 1:  
 ```javascript
-
-module.exports = {
-  
+module.exports = {  
     reactiveData   : {
         tasks: function () {
             return Tasks.find().fetch()
         }
     },
-
     created: function(){
-        this.subscribe('tasks/all');
+        this.subscribe('tasks/all')
     }
-}
-    
+}    
 ```
 
 
 Usage example 2:
-
 ```javascript    
 module.exports = {
     created: function(){
-        this.subscribe('tasks/all');
+        this.subscribe('tasks/all')
         this.autorun(function(){
             this.$set('tasks', Tasks.find().fetch())
         }.bind(this))
@@ -44,3 +37,30 @@ module.exports = {
 }
 ```
  
+ 
+Usage example 3 with VueRouter:
+```javascript
+module.exports = {  
+    reactiveData   : {
+        tasks: function () {
+            return Tasks.find(this.$route.params.id).fetch()
+        }
+    },
+    created: function(){
+        this.subscribe('tasks/all')
+    }
+}
+```
+
+
+Usage example 4:
+```javascript
+module.exports = {  
+    reactiveData   : {
+        tasks: function (vm) {
+            console.log(vm === this) //returns true
+        }
+    }
+}
+```
+
--- vue-meteor-data.js
+++ vue-meteor-data.js
@@ -3,7 +3,7 @@
  *
  * Installation:
  *     var VueMeteorData = require ('vue-meteor-data');
- *     Vue.use(VueMeteorData);
+ *     Vue.mixin(VueMeteorData)
  *
  * Usage example 1:
  *     module.exports = {
@@ -29,96 +29,75 @@
  *   }
  */
 
-(function () {
-  var vue // lazy bind
+module.exports =  {
 
-  var meteorData = {
+  created: function () {
+    var VueComponent = this;
+    this._meteorHandles = [];
 
-    created: function () {
-      var VueComponent = this;
-      this._meteorHandles = [];
-
-      /**
-       * Wrap all queries in Tracker.autorun
-       * Usage example:
-       *     reactiveData: {
+    /**
+     * Wrap all queries in Tracker.autorun
+     * Usage example:
+     *     reactiveData: {
        *       tasks: function (component) {
        *         this.subscribe('myPubliation');
        *         return Tasks.find().fetch()
        *       }
        *     }
-       */
-      if (reactiveData = this.$options.reactiveData) {
-        for (var key of Object.keys(reactiveData)) {
-          var reactiveFunction = reactiveData[ key ];
-          var handle = Tracker.autorun(function () {
-            VueComponent.$set(key, reactiveFunction(VueComponent));
-          })
-          VueComponent._meteorHandles.push(handle);
-        }
+     */
+    var reactiveData = this.$options.reactiveData
+    if (reactiveData) {
+      for (var key of Object.keys(reactiveData)) {
+        var reactiveFunction = reactiveData[ key ].bind(VueComponent);
+        var handle = Tracker.autorun(function () {
+          VueComponent.$set(key, reactiveFunction(VueComponent));
+        })
+        VueComponent._meteorHandles.push(handle);
       }
-    },
+    }
+  },
 
-    destroyed: function () {
-      //Stop all Meteor reactivity when view is destroyed.
-      this._meteorHandles.forEach(function (sub) {
-        sub.stop();
-      })
-    },
+  destroyed: function () {
+    //Stop all Meteor reactivity when view is destroyed.
+    this._meteorHandles.forEach(function (sub) {
+      sub.stop();
+    })
+  },
 
 
-    methods: {
-      /**
-       * Subscription that automatically stop when view is destroyed
-       * Usage example:
-       *     created: function(){
+  methods: {
+    /**
+     * Subscription that automatically stop when view is destroyed
+     * Usage example:
+     *     created: function(){
        *       this.subscription("myPublication")
        *     }
-       * @returns {*}
-       */
-      subscribe: function () {
-        var handle = Meteor.subscribe.apply(this, arguments);
-        this._meteorHandles.push(handle);
-        return handle;
-      },
+     * @returns {*}
+     */
+    subscribe: function () {
+      var handle = Meteor.subscribe.apply(this, arguments);
+      this._meteorHandles.push(handle);
+      return handle;
+    },
 
 
-      /**
-       * Autorun that automatically stops when view is destroyed
-       *
-       * Usage example:
-       *     created: function(){
+    /**
+     * Autorun that automatically stops when view is destroyed
+     *
+     * Usage example:
+     *     created: function(){
        *       this.autorun(function(){
        *         this.myvar = myReactiveFunction();
        *       }.bind(this)
        *     }
-       * @returns {Tracker.Computation}
-       */
-      autorun: function () {
-        var handle = Tracker.autorun.apply(this, arguments);
-        this._meteorHandles.push(handle);
-        return handle;
-      }
-
+     * @returns {Tracker.Computation}
+     */
+    autorun: function () {
+      var handle = Tracker.autorun.apply(this, arguments);
+      this._meteorHandles.push(handle);
+      return handle;
     }
 
   }
 
-
-  var api = {
-    mixin  : meteorData,
-    install: function (Vue) {
-      Vue.options = Vue.util.mergeOptions(Vue.options, meteorData)
-    }
-  }
-
-  if (typeof exports === 'object' && typeof module === 'object') {
-    module.exports = api
-  } else if (typeof define === 'function' && define.amd) {
-    define(function () {
-      return api
-    })
-  } else if (typeof window !== 'undefined') {
-    window.VueMeteorData = api
-  }
-})()
+}
